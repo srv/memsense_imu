a4b5fc638d31149247dd7020cae124b3de5e31e2
Joan Pau Beltran <joanpau.beltran@uib.cat>
2011-09-22 20:57:52 +0200

    Moved IMUNodelet into memsense_imu namespace.

--- src/nodes/imu_nodelet.cpp
+++ src/nodes/imu_nodelet.cpp
@@ -1,42 +1,43 @@
-/** @file
- *
- * @brief ROS Memsense IMU generic driver (nodelet version)
+/**
+ * @file
+ * @brief ROS Memsense IMU generic driver (nodelet version).
  *
  * @par Advertises
  *
- * - @b imu/data topic (sensor_msgs/Imu) IMU's raw data
+ * - @b imu/data topic (sensor_msgs/Imu)
+ *   IMU's raw data.
  *
- * - @b imu/data_calibrated topic (sensor_msgs/Imu) Calibrated (bias removed)
- *   IMU data
+ * - @b imu/data_calibrated topic (sensor_msgs/Imu)
+ *   Calibrated (bias removed) IMU data.
  *
- * - @b imu/data_filtered topic (sensor_msgs/Imu) IMU's filtered output
- *  (mean value every sec seconds)
+ * - @b imu/data_filtered topic (sensor_msgs/Imu)
+ *   IMU's filtered output (mean value every sec seconds).
  *
- * - @b imu/data_filtered_calibrated topic (sensor_msgs/Imu) Calibrated (bias removed)
- *   filtered (mean every sec seconds) IMU data
+ * - @b imu/data_filtered_calibrated topic (sensor_msgs/Imu)
+ *   Calibrated (bias removed) filtered (mean every sec seconds) IMU data.
  *
  * @par Parameters
  *
- * - @b ~imu_type Memsense device type (default nIMU_3temp)
+ * - @b ~imu_type Memsense device type (default nIMU_3temp).
  *
- * - @b ~gyro_range gyroscop's range (default 150.0 degrees per second)
- * - @b ~accel_range accelerometer range (default 2.0 g's)
- * - @b ~mag_range magnetometer range (default 1.9 gauss)
+ * - @b ~gyro_range gyroscop's range (default 150.0 degrees per second).
+ * - @b ~accel_range accelerometer range (default 2.0 g's).
+ * - @b ~mag_range magnetometer range (default 1.9 gauss).
 
- * - @b ~serial_port Serial port device file name (default /dev/ttyUSB0)
+ * - @b ~serial_port Serial port device file name (default /dev/ttyUSB0).
 
- * - @b ~gyro_var gyroscope's variance (default 0.0)
- * - @b ~accel_var accelerometer's variance (default 0.0)
- * - @b ~mag_var magnetometer's variance (default 0.0)
+ * - @b ~gyro_var gyroscope's variance (default 0.0).
+ * - @b ~accel_var accelerometer's variance (default 0.0).
+ * - @b ~mag_var magnetometer's variance (default 0.0).
  *
- * - @b ~gyro_bias_(x|y|z) gyroscope's bias in each axis (default 0.0)
- * - @b ~accel_bias_(x|y|z) accelerometer's bias in each axis (default 0.0)
- * - @b ~mag_bias_(x|y|z) magnetometer's bias in each axis (default 0.0)
+ * - @b ~gyro_bias_(x|y|z) gyroscope's bias in each axis (default 0.0).
+ * - @b ~accel_bias_(x|y|z) accelerometer's bias in each axis (default 0.0).
+ * - @b ~mag_bias_(x|y|z) magnetometer's bias in each axis (default 0.0).
  *
- * - @b ~filter_rate filtered output rate (IMU samples in the interval are collected
- *   and the output is its mean)
+ * - @b ~filter_rate filtered output rate
+ *   (IMU samples in the interval are collected and the output is its mean).
  *
- * - @b ~frame_id frame identifier for message header
+ * - @b ~frame_id frame identifier for message header.
  *
  */
 
@@ -47,6 +48,8 @@
 
 #include "imu_node_base.h"
 
+namespace memsense_imu
+{
 
 /** Nodelet interface for IMU driver.
  */
@@ -59,14 +62,15 @@ private:
   boost::shared_ptr<memsense_imu::IMUNodeBase> imu_node_;
 };
 
+} // namespace
 
-IMUNodelet::IMUNodelet()
+memsense_imu::IMUNodelet::IMUNodelet()
 {}
 
 /** Nodelet initialization.
  *  @note Must return immediately.
  */
-void IMUNodelet::onInit()
+void memsense_imu::IMUNodelet::onInit()
 {
   ros::NodeHandle nh(getNodeHandle(),"imu");
   ros::NodeHandle priv(getPrivateNodeHandle());
@@ -85,4 +89,4 @@ void IMUNodelet::onInit()
 // Register this plugin with pluginlib.
 // Names must match *nodelet.xml in package root.
 // Parameters are: package, class name, class type, base class type
-PLUGINLIB_DECLARE_CLASS(memsense_imu, imu_nodelet, IMUNodelet, nodelet::Nodelet);
+PLUGINLIB_DECLARE_CLASS(memsense_imu, imu_nodelet, memsense_imu::IMUNodelet, nodelet::Nodelet);
